52efd4bf720f91ad5c0ecf018be4e2f3e116e1ae
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-02 13:15:11 -0800

    search a char util function

--- utils.c
+++ utils.c
@@ -18,3 +18,27 @@ int _strcmp(char *s1, char *s2)
 		return (-1);
 	return (0);
 }
+
+/**
+ * _sch - search if a char is inside a string
+ * @s: string to review
+ * @c: char to find
+ * Return: 1 if success 0 if not
+ */
+int _sch(char *s, char c)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == c)
+		{
+			break;
+		}
+		cont++;
+	}
+	if (s[cont] == c)
+		return (1);
+	else
+		return (0);
+}
