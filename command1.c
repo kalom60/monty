3e684a10a97b661d216ee49e6d069e8c1c1bd354
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-02 12:25:47 -0800

    pall and pint commands

--- command1.c
+++ command1.c
@@ -37,3 +37,46 @@ void _push(stack_t **doubly, unsigned int cline)
     else
         add_dnodeint_end(doubly, n);
 }
+
+/**
+ * _pall - prints all values on the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line numbers
+ * Return: no return
+ */
+void _pall(stack_t **doubly, unsigned int cline)
+{
+    stack_t *aux;
+    (void)cline;
+
+    aux = *doubly;
+
+    while (aux)
+    {
+        printf("%d\n", aux->n);
+        aux = aux->next;
+    }
+}
+
+/**
+ * _pint - prints the value at the top of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pint(stack_t **doubly, unsigned int cline)
+{
+    (void)cline;
+
+    if (*doubly == NULL)
+    {
+        dprintf(2, "L%u: ", cline);
+        dprintf(2, "can't pint, stack empty\n");
+        free_allin();
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*doubly)->n);
+}
