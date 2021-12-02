599c3fda3c66932efc576807ef6533f9206f5c72
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-02 12:43:58 -0800

    Betty check

--- command1.c
+++ command1.c
@@ -9,33 +9,33 @@
  */
 void _push(stack_t **doubly, unsigned int cline)
 {
-    int n, j;
+	int n, j;
 
-    if (!allin.arg)
-    {
-        dprintf(2, "L%u: ", cline);
-        dprintf(2, "usage: push integer\n");
-        free_allin();
-        exit(EXIT_FAILURE);
-    }
+	if (!allin.arg)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_allin();
+		exit(EXIT_FAILURE);
+	}
 
-    for (j = 0; allin.arg[j] != '\0'; j++)
-    {
-        if (!isdigit(allin.arg[j]) && allin.arg[j] != '-')
-        {
-            dprintf(2, "L%u: ", cline);
-            dprintf(2, "usage: push integer\n");
-            free_allin();
-            exit(EXIT_FAILURE);
-        }
-    }
+	for (j = 0; allin.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(allin.arg[j]) && allin.arg[j] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_allin();
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    n = atoi(allin.arg);
+	n = atoi(allin.arg);
 
-    if (allin.lifo == 1)
-        add_dnodeint(doubly, n);
-    else
-        add_dnodeint_end(doubly, n);
+	if (allin.lifo == 1)
+		add_dnodeint(doubly, n);
+	else
+		add_dnodeint_end(doubly, n);
 }
 
 /**
@@ -47,16 +47,16 @@ void _push(stack_t **doubly, unsigned int cline)
  */
 void _pall(stack_t **doubly, unsigned int cline)
 {
-    stack_t *aux;
-    (void)cline;
+	stack_t *aux;
+	(void)cline;
 
-    aux = *doubly;
+	aux = *doubly;
 
-    while (aux)
-    {
-        printf("%d\n", aux->n);
-        aux = aux->next;
-    }
+	while (aux)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+	}
 }
 
 /**
@@ -68,17 +68,17 @@ void _pall(stack_t **doubly, unsigned int cline)
  */
 void _pint(stack_t **doubly, unsigned int cline)
 {
-    (void)cline;
+	(void)cline;
 
-    if (*doubly == NULL)
-    {
-        dprintf(2, "L%u: ", cline);
-        dprintf(2, "can't pint, stack empty\n");
-        free_allin();
-        exit(EXIT_FAILURE);
-    }
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_allin();
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%d\n", (*doubly)->n);
+	printf("%d\n", (*doubly)->n);
 }
 
 /**
@@ -90,17 +90,17 @@ void _pint(stack_t **doubly, unsigned int cline)
  */
 void _pop(stack_t **doubly, unsigned int cline)
 {
-    stack_t *aux;
+	stack_t *aux;
 
-    if (doubly == NULL || *doubly == NULL)
-    {
-        dprintf(2, "L%u: can't pop an empty stack\n", cline);
-        free_allin();
-        exit(EXIT_FAILURE);
-    }
-    aux = *doubly;
-    *doubly = (*doubly)->next;
-    free(aux);
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", cline);
+		free_allin();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	free(aux);
 }
 
 /**
@@ -112,25 +112,25 @@ void _pop(stack_t **doubly, unsigned int cline)
  */
 void _swap(stack_t **doubly, unsigned int cline)
 {
-    int m = 0;
-    stack_t *aux = NULL;
+	int m = 0;
+	stack_t *aux = NULL;
 
-    aux = *doubly;
+	aux = *doubly;
 
-    for (; aux != NULL; aux = aux->next, m++)
-        ;
+	for (; aux != NULL; aux = aux->next, m++)
+		;
 
-    if (m < 2)
-    {
-        dprintf(2, "L%u: can't swap, stack too short\n", cline);
-        free_allin();
-        exit(EXIT_FAILURE);
-    }
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", cline);
+		free_allin();
+		exit(EXIT_FAILURE);
+	}
 
-    aux = *doubly;
-    *doubly = (*doubly)->next;
-    aux->next = (*doubly)->next;
-    aux->prev = *doubly;
-    (*doubly)->next = aux;
-    (*doubly)->prev = NULL;
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
 }
