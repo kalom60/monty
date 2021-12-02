1bb877c654b1f2a5f996c582f4935d15588c5dea
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-02 13:17:59 -0800

    Betty check and string token util function

--- utils.c
+++ utils.c
@@ -42,3 +42,44 @@ int _sch(char *s, char c)
 	else
 		return (0);
 }
+
+/**
+ * _strtoky - function that cut a string into tokens depending of the delimit
+ * @s: string to cut in parts
+ * @d: delimiters
+ * Return: first partition
+ */
+char *_strtoky(char *s, char *d)
+{
+	static char *tko;
+	int i = 0, j = 0;
+
+	if (!s)
+		s = tko;
+	while (s[i] != '\0')
+	{
+		if (_sch(d, s[i]) == 0 && s[i + 1] == '\0')
+		{
+			tko = s + i + 1;
+			*tko = '\0';
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 0)
+			i++;
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 1)
+		{
+			tko = s + i + 1;
+			*tko = '\0';
+			tko++;
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 1)
+		{
+			j++;
+			i++;
+		}
+	}
+	return (NULL);
+}
